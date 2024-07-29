3f4c43a975337a35e34d851bd7e1bfcf420d0351
leowjli <leo.wj.li@gmail.com>
2024-07-29 16:15:43 -0700

    updated all fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
         'grey': '#F1F2F2',
       }
     },
+    fontFamily: {
+      'bogart': ['bogart', 'sans-serif'],
+      'bogart-bold': ['bogart-bold', 'sans-serif'],
+      'bogart-italic': ['bogart-italic', 'sans-serif'],
+      'bogart-thin': ['bogart-thin', 'sans-serif'],
+      'genty': ['genty', 'serif'],
+      'tt_firs_neue': ['tt_firs_neue', 'san-serif'],
+      'tt_firs_neue-thin': ['tt_firs_neue-thin', 'san-serif'],
+    }
   },
   plugins: [],
 }
